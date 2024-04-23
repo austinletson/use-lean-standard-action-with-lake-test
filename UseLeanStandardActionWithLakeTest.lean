93b2a76975c8af076ac7e6b3dbef57e766fa10ce
austinletson <waustinletson@gmail.com>
2024-04-23 18:05:11 -0400

    init

---
+++ UseLeanStandardActionWithLakeTest.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `UseLeanStandardActionWithLakeTest` library.
+-- Import modules here that should be built as part of the library.
+import «UseLeanStandardActionWithLakeTest».Basic
---
+++ lakefile.lean
@@ -0,0 +1,18 @@
+import Lake
+open Lake DSL
+
+package «use-lean-standard-action-with-lake-test» where
+  -- add package configuration options here
+
+lean_lib «UseLeanStandardActionWithLakeTest» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «use-lean-standard-action-with-lake-test» where
+  root := `Main
+
+@[test_runner]
+script test do
+  println! "Running fake tests..."
+  println! "Fake tests passed!"
+  return 0

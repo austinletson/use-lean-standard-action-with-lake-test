a52e7d51f7ceaca258dbce2573ef13eac98a8add
Austin Letson <waustinletson@gmail.com>
2024-08-21 11:42:37 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -15,10 +15,10 @@ lean_exe «use-lean-standard-action-with-lake-test» where
  script test do
   println! "Running fake tests..."
   println! "Fake tests passed!"
-  return 1
+  return 0
 
 @[lint_driver]
  script lint do
   println! "Running fake lints..."
-  println! "Fake lints passed!"
-  return 0
+  println! "Fake lints failed!"
+  return 1
